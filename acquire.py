5a2598612c731c4dfe6424facf43010d75a4f592
Ray Cerna <ray.cerna01@gmail.com>
2022-05-20 16:27:57 -0500

    This file contains the necessary functions to acquire Telco data from Codeup DB

--- acquire.py
+++ acquire.py
@@ -3,6 +3,14 @@ import numpy as np
 import os
 from env import host, user, password
 
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the Codeup db.
+    It takes in a string name of a database as an argument.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
 def new_telco_data():
     '''
     This function reads the telco data from the Codeup db into a df.
