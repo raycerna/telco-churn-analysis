6d916373d456b5ec3f2e2eed09c15f9b74af50b5
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-05-25 00:17:20 -0500

    prepare file

--- prepare.py
+++ prepare.py
@@ -92,10 +92,11 @@ def prep_telco_data(df):
     #df['churn_encoded'] = df.churn.map({'Yes': 1, 'No': 0})
     
     # Get dummies for non-binary categorical variables
-    dummy_df = pd.get_dummies(telco_df.gender, drop_first=True)
+    dummy_df = pd.get_dummies(df.gender, drop_first=True)
     
     # Concatenate dummy dataframe to original 
     df = pd.concat([df, dummy_df], axis=1)
+    
     return df
 
     # split the data
