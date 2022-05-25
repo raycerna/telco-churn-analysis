cc05e618443c7c36147d4d0dccece133d6891231
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-05-25 10:32:03 -0500

    prepare

--- prepare.py
+++ prepare.py
@@ -1,7 +1,7 @@
 import math
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
