3c628842f6e7af932c340afa01f96fb40e4d9a9f
Ray Cerna <102126242+raycerna@users.noreply.github.com>
2022-05-25 10:05:26 -0500

    prepare

--- prepare.py
+++ prepare.py
@@ -104,15 +104,11 @@ def prep_telco_data(df):
     # #return train, validate, test
 
 
-def percentage_stacked_plot(columns_to_plot, super_title):
+def percentage_stacked_plot(columns_to_plot, title):
     
     '''
-    Prints a 100% stacked plot of the response variable for independent variable of the list columns_to_plot.
-            Parameters:
-                    columns_to_plot (list of string): Names of the variables to plot
-                    super_title (string): Super title of the visualization
-            Returns:
-                    None
+    Returns a 100% stacked plot of the response variable for independent variable of the list columns_to_plot.
+    Parameters: columns_to_plot (list of string): Names of the variables to plot
     '''
     
     number_of_columns = 2
@@ -120,7 +116,7 @@ def percentage_stacked_plot(columns_to_plot, super_title):
 
     # create a figure
     fig = plt.figure(figsize=(12, 5 * number_of_rows)) 
-    fig.suptitle(super_title, fontsize=22,  y=.95)
+    fig.suptitle(title, fontsize=22,  y=.95)
  
 
     # loop to each column name to create a subplot
@@ -134,19 +130,15 @@ def percentage_stacked_plot(columns_to_plot, super_title):
         prop_by_independent = pd.crosstab(telco_df[column], telco_df['churn']).apply(lambda x: x/x.sum()*100, axis=1)
 
         prop_by_independent.plot(kind='bar', ax=ax, stacked=True,
-                                 rot=0, color=['green','red'])
+                                 rot=0, color=['#0072BD','pink'])
 
         # set the legend in the upper right corner
         ax.legend(loc="upper right", bbox_to_anchor=(0.62, 0.5, 0.5, 0.5),
                   title='Churn', fancybox=True)
 
-        # set title and labels
-        ax.set_title('Proportion of observations by ' + column,
-                     fontsize=16, loc='left')
-
-        ax.tick_params(rotation='auto')
-
         # eliminate the frame from the plot
         spine_names = ('top', 'right', 'bottom', 'left')
         for spine_name in spine_names:
             ax.spines[spine_name].set_visible(False)
+
+    return percentage_stacked_plot
